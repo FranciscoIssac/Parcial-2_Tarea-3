92b38ba99853e0113798b079422d5ff0f7a82415
Francisco Issac <a276679@alumnos.uaslp.mx>
2022-03-10 17:56:40 -0600

    Tarea 3 - Examen 1

---
+++ src/uaslp/objetos/parcial1/examen/pos/PointOfSale.java
@@ -0,0 +1,47 @@
+package uaslp.objetos.parcial1.examen.pos;
+
+import java.util.ArrayList;
+
+public class PointOfSale {
+    private ArrayList<CartItem> items;
+
+    public PointOfSale() {
+        items = new ArrayList<>();
+    }
+
+    public void addToCart(int quantity, Product product) {
+        for(CartItem item: items) {
+            if(item.getProductName().equals(product.getName())) {
+                item.setQuantity(item.getQuantity() + quantity);
+                return;
+            }
+        }
+
+        CartItem cartItem = new CartItem();
+        cartItem.setQuantity(quantity);
+        cartItem.setProduct(product);
+        items.add(cartItem);
+    }
+
+    public float getTotal() {
+        float total = 0;
+
+        for(CartItem item: items) {
+            total += item.getAmount() * item.getQuantity();
+        }
+
+        return total;
+    }
+
+    public ArrayList<CartItem> getProducts() {
+        return items;
+    }
+
+    public int getCountByName(String name) {
+        for(CartItem item: items) {
+            if(name.equals(item.getProductName()))
+                return item.getQuantity();
+        }
+        return 0;
+    }
+}
